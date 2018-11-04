09115203144b966bc835d6265aedd421b3cc67fe
Iesha Holy <iesha@balancegroupinc.com>
2018-11-04 21:04:41 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -7,14 +7,14 @@
 
 name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
-  name.each do |name|
-  puts "Hello, my name is #{name}."
-end
+#   name.each do |name|
+#   puts "Hello, my name is #{name}."
+# end
 
 counter = 0 
-# while counter < 8
-#   puts "Hello, #{name[counter]}! You'll be assigned to room #{room}!"
-#   counter = counter + 1 
-# end
+ while counter < 8
+   puts "Hello, #{name[counter]}! You'll be assigned to room #{room}!"
+   counter = counter + 1 
+ end
 
 
