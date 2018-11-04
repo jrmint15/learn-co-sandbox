346d650128724730cf9c6f6538f54f2d044da0c7
Iesha Holy <iesha@balancegroupinc.com>
2018-11-04 20:48:45 +0000

    automatically backed up by learn

--- challenge.rb
+++ challenge.rb
@@ -11,9 +11,8 @@ name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
   puts "Hello, my name is #{name}."
 end
 
-# def batch_badge_creator(name)
-#   puts "Hello, my name is #{name}."
-# end
+def assign_rooms
+  
 
 
 
