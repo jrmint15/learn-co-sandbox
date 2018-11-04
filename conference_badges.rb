280a33ddbb6272809430293bec4e319cf20c9668
Iesha Holy <iesha@balancegroupinc.com>
2018-11-04 21:02:05 +0000

    automatically backed up by learn

---
+++ conference_badges.rb
@@ -0,0 +1,13 @@
+# def badge_maker(name)
+#   puts "Hello, my name is #{name}."
+# end
+
+# name = "Iesha"
+# badge_maker(name)
+
+name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+
+  name.each do |name|
+  puts "Hello, my name is #{name}."
+end
+
