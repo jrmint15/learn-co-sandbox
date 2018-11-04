81fada34a7e18fdc013e2a28bc25384adf79c530
Iesha Holy <iesha@balancegroupinc.com>
2018-11-04 21:05:14 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,10 +11,13 @@ name = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 #   puts "Hello, my name is #{name}."
 # end
 
+room = (1..7)
+
 counter = 0 
  while counter < 8
    puts "Hello, #{name[counter]}! You'll be assigned to room #{room}!"
    counter = counter + 1 
  end
+ 
 
 
